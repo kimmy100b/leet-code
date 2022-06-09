5cd292ab91ed31bd9d504e4a2e538f0896e65e6c
신윤정 <kimmy100b@gmail.com>
2022-06-09 10:25:08 +0900

    28. Implement strStr()

---
+++ easy/implementStrstr.js
@@ -0,0 +1,17 @@
+/**
+
+  날짜 : 2022.06.09
+  난이도 : Easy
+  제목 : 28. Implement strStr()
+  URL : https://leetcode.com/problems/implement-strstr/
+
+*/
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  return haystack.indexOf(needle);
+};
